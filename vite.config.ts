7fa076f582025348e12c384d6e29acc2da075520
Cristian David Home <cristian_david033@hotmail.com>
2024-08-27 02:17:15 -0500

    Update base URL to '/raffles-app/' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ms-app/',
+  base: '/raffles-app/',
   plugins: [
     Icons({ compiler: 'vue3' }),
     vue(),
